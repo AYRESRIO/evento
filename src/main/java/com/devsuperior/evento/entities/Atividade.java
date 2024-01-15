1a1332842775aa0ed7a6e0afa6a4468558731dc9
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2024-01-15 01:11:03 -0300

    Created class Bloco;Modified classes:Atividade,Participantes.

--- src/main/java/com/devsuperior/evento/entities/Atividade.java
+++ src/main/java/com/devsuperior/evento/entities/Atividade.java
@@ -2,6 +2,11 @@ package com.devsuperior.evento.entities;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Entity
 @Table(name = "tb_atividade")
 public class Atividade {
@@ -9,11 +14,22 @@ public class Atividade {
 @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
-    private String descricao;
+   @Column(columnDefinition = "TEXT")
+   private String descricao;
     private Double preco;
     @ManyToOne
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
+    @ManyToMany
+    @JoinTable(name = "tb_atividade_participante",
+            joinColumns = @JoinColumn(name = "atividade_id"),
+            inverseJoinColumns = @JoinColumn(name = "participante_id"))
+
+    private Set<Participante> participantes = new HashSet<>();
+
+    @OneToMany(mappedBy = "atividade")
+    private List<Bloco> blocos = new ArrayList<>();
+
 
     public Atividade() {
     }
@@ -64,5 +80,13 @@ public class Atividade {
 
     public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
+    }
+
+    public Set<Participante> getParticipantes() {
+        return participantes;
+    }
+
+    public List<Bloco> getBlocos() {
+        return blocos;
     }
 }
