7d25073560d3d83bce910b3d7710bc736047745b
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2024-01-16 01:17:37 -0300

    modified class Categoria and file import.sql.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,4 +13,13 @@ INSERT INTO tb_bloco (inicio, fim) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-25T
 INSERT INTO tb_bloco (inicio, fim) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-25T14:00:00Z', '2017-09-25T18:00:00Z');
 INSERT INTO tb_bloco (inicio, fim) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-26T08:00:00Z', '2017-09-26T11:00:00Z');
 
+INSERT INTO tb_atividade_participante (atividade_id,participante_id) VALUES(1,1);
+INSERT INTO tb_atividade_participante (atividade_id,participante_id) VALUES(1,2);
+INSERT INTO tb_atividade_participante (atividade_id,participante_id) VALUES(1,3);
+INSERT INTO tb_atividade_participante (atividade_id,participante_id) VALUES(2,1);
+INSERT INTO tb_atividade_participante (atividade_id,participante_id) VALUES(2,3);
+INSERT INTO tb_atividade_participante (atividade_id,participante_id) VALUES(2,4);
+
+
+
 
